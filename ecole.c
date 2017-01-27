e893348a27a1c9e53f8b983e49414f1733a5e0bc
elfedac <elfedac@gmail.com>
2017-01-27 10:58:17 +0100

    V3
    Fonctions complétées avec l'ajout de la structure adresse (rue, code postal, ville, tel, mail)
    Fonction Saisie parfaitement fonctionnelle
    Fichier d'initialisation de l'Ecole à ne pas écraser "fichier.ecole.csv"
    Fonction d'affichage à corriger, impossible d'ouvrir "fichier.ecole.csv" - erreur

--- ecole.c
+++ ecole.c
@@ -1,6 +1,37 @@
-
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include "ecole.h"
+
+
+//Fonction Saisie Adresse
+void SaisirAdresse(Adresse_t *A)
+{
+    printf("\nSaisir la rue : ");
+    fgets(A->rue, RUEMAX, stdin);
+    A->rue[strlen(A->rue)-1]='\0'; // on supprime le '\n' généré par fgets
+
+    printf("\nSaisir le code postal de la commune : ");
+    scanf("%d", &A->CodePostal);
+    getchar();
+
+    printf("\nSaisir la ville : ");
+    fgets(A->Ville, VILLEMAX, stdin);
+    A->Ville[strlen(A->Ville)-1]='\0'; // on supprime le '\n' généré par fgets
+
+    printf("\nSaisir numero de telephone fixe:");
+    fgets(A->TelMaison, TELMAX, stdin);
+    A->TelMaison[strlen(A->TelMaison)-1]='\0'; // on supprime le '\n' généré par fgets
+
+    printf("\nSaisir numero de telephone mobile:");
+    fgets(A->TelMobile, TELMAX, stdin);
+    A->TelMobile[strlen(A->TelMobile)-1]='\0'; // on supprime le '\n' généré par fgets
+
+    printf("\nSaisir adresse mail : ");
+    fgets(A->Mail, MAILMAX, stdin);
+    A->Mail[strlen(A->Mail)-1]='\0'; // on supprime le '\n' généré par fgets
+
+}
 
 
 //Fonction Affichage Ecole depuis un fichier .csv qui se nomme ici "fichier_ecole.csv"
@@ -14,66 +45,106 @@ void AfficherEcole(const char *nom_fichier, Ecole_t *Ec)
 
     FILE *ptr_fichierEcole; // création d'un pointeur FILE
 
-    ptr_fichierEcole = fopen(nom_fichier, "r"); // ouverture du fichier en lecture
+    ptr_fichierEcole=fopen("nom_fichier", "r"); // ouverture du fichier en lecture
 
-    if(!ptr_fichier) // si l'ouverture se passe mal
+    if(!ptr_fichierEcole) // si l'ouverture se passe mal
     {
         perror("erreur ouverture du fichier");
         exit(EXIT_FAILURE);
     }
     else
     {
-        fgets(ligne, LIGNE, ptr_fichierEcole); // lecture ligne
 
-        champ=strtok(ligne, ";"); // mettre dans champ les éléments de ligne jusqu'au prochain ';'
-        strcpy(Ec->nomEcole, champ); // copier champ dans le nom de l'etablissement
+        fgets(ligne, LIGNE, ptr_fichierEcole);  // lecture ligne
+
+        champ=strtok(ligne, ";");               // mettre dans champ les éléments de ligne jusqu'au prochain ';'
+        strcpy(Ec->nomEcole, champ);            // copie champ dans le nom de l'etablissement
 
 
-        champ=strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
-        strcpy(Ec->nomDirecteur, champ); // copier champ dans le nom du directeur(trice)
+        champ=strtok(NULL, ";");                // mettre dans champ les éléments de ligne jusqu'au prochain ';'
+        strcpy(Ec->nomDirecteur, champ);        // copie champ dans le nom du directeur(trice)
 
-        champ=strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
-        strcpy(Ec->prenomDirecteur, champ); // copier champ dans le prenom du directeur(trice)
+        champ=strtok(NULL, ";");                // mettre dans champ les éléments de ligne jusqu'au prochain ';'
+        strcpy(Ec->prenomDirecteur, champ);     // copie champ dans le prenom du directeur(trice)
 
-        champ=strtok(NULL, ";");// mettre dans champ les éléments de ligne jusqu'au prochain ';'
-        Ec->nbclasse=atoi(champ); // on convertit le champ en un entier pour le nombre de classes
+        champ=strtok(NULL, ";");                // mettre dans champ les éléments de ligne jusqu'au prochain ';'
+        Ec->nbclasse=atoi(champ);               // on convertit le champ en un entier pour le nombre de classes
+
+        champ=strtok(ligne, ";");
+        strcpy(Ec->AdresseEcole.rue, champ);
+
+        champ=strtok(NULL, ";");
+        Ec->AdresseEcole.CodePostal=atoi(champ);
+
+        champ=strtok(ligne, ";");
+        strcpy(Ec->AdresseEcole.Ville, champ);
+
+        champ=strtok(ligne, ";");
+        strcpy(Ec->AdresseEcole.TelMaison, champ);
+
+        champ=strtok(ligne, ";");
+        strcpy(Ec->AdresseEcole.TelMobile, champ);
+
+        champ=strtok(ligne, ";");
+        strcpy(Ec->AdresseEcole.Mail, champ);
 
     }
 
-fclose(ptr_fichierEcole); // fermeture du fichier .csv
+fclose(ptr_fichierEcole);                        // fermeture du fichier .csv
 
+//Instructions d'affichage des caractéristiques de l'Etablissement
+
+printf("Nom de l'Etablissement : ", Ec->nomEcole);
+printf("Directeur de l'Etablissement\n");
+printf("\tNom: ", Ec->nomDirecteur);
+printf("\n\tPrenom: ", Ec->prenomDirecteur);
+printf("\nRue: ", Ec->AdresseEcole.rue);
+printf("\nCode Postal: ", Ec->AdresseEcole.CodePostal);
+printf("\nVille: ", Ec->AdresseEcole.Ville);
+printf("\nVille: ", Ec->AdresseEcole.TelMaison);
+printf("\nVille: ", Ec->AdresseEcole.TelMobile);
+printf("\nVille: ", Ec->AdresseEcole.Mail);
+
+printf("\nNombre de classes: ", Ec->nbclasse);
 }
 
 
 //Fonction Saisie Ecole
-void SaisirEcole(Ecole_t &Ec)
+void SaisirEcole(Ecole_t *Ec)
 {
     printf("********** Saisie des informations Etablissement **********\n\n\n");
 
     printf("Saisir le nom de l'etablissement: ");
     fgets(Ec->nomEcole, NOMMAXECOLE, stdin);
+    Ec->nomEcole[strlen(Ec->nomEcole)-1]='\0';              // on supprime le '\n' généré par fgets
 
     printf("\nSaisir le nom du Directeur(trice): ");
     fgets(Ec->nomDirecteur, NOMMAX, stdin);
+    Ec->nomDirecteur[strlen(Ec->nomDirecteur)-1]='\0';      // on supprime le '\n' généré par fgets
 
     printf("\nSaisir le prenom du Directeur(trice): ");
     fgets(Ec->prenomDirecteur, PRENOMMAX, stdin);
+    Ec->prenomDirecteur[strlen(Ec->prenomDirecteur)-1]='\0';// on supprime le '\n' généré par fgets
 
+    SaisirAdresse(&Ec->AdresseEcole);
 
     FILE *ptr_fichierEcole; // création d'un pointeur FILE
 
-    ptr_fichierEcole = fopen("fichier_ecole.csv", "w"); // ouverture du fichier secret en mode "écriture"
+    ptr_fichierEcole = fopen("fichier_ecole.csv", "w");     // ouverture du fichier secret en mode "écriture"
 
-    if(!ptr_fichierEcole)                               // si l'ouverture se passe mal
+    if(!ptr_fichierEcole)                                   // si l'ouverture se passe mal
     {
-        perror("erreur ouverture du fichier");
-        exit(EXIT_FAILURE);
+        perror("erreur ouverture du fichier");              //Affichage du message d'erreur
+        exit(EXIT_FAILURE);                                 //Sortie du programme
     }
     else
     {
         // écriture des caractéristiques de l'établissement dans un fichier csv compatible LibreOffice/Microsoft Office
-        fprintf(ptr_fichierEcole, "%s ; %s ; %s ; %d ; ", Ec->nomEcole, Ec->nomDirecteur, Ec->prenomDirecteur, Ec->nbclasse);
+        fprintf(ptr_fichierEcole, "%s;%s;%s;%d;", Ec->nomEcole, Ec->nomDirecteur, Ec->prenomDirecteur, Ec->nbclasse);
+
+        fprintf(ptr_fichierEcole, "%s ; %d ; %s ; %s ; %s ; %s", Ec->AdresseEcole.rue, Ec->AdresseEcole.CodePostal, Ec->AdresseEcole.Ville, Ec->AdresseEcole.TelMaison,
+                                                                 Ec->AdresseEcole.TelMobile, Ec->AdresseEcole.Mail);
     }
 
-fclose(ptr_fichierEcole); // fermeture du fichier .csv
+fclose(ptr_fichierEcole);                                   // fermeture du fichier .csv
 }
--- ecole.h
+++ ecole.h
@@ -10,6 +10,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "adresse.h"
+
 
 #ifndef ECOLE_H
 #define ECOLE_H
@@ -17,10 +19,10 @@
 #define NOMMAX 30
 #define PRENOMMAX 20
 #define NOMMAXECOLE 50
-#define NOMCLASSE 5
 #define LIGNE 600
 #define RUEMAX 60
 #define VILLEMAX 30
+#define TELMAX 14
 #define MAILMAX 50
 #define CLEAR()     system("clear");
 
@@ -29,13 +31,13 @@
 //structure pour l'Ecole
 typedef struct Ecole
 {
-char nomEcole[NOMMAXECOLE];     //
-char nomDirecteur [NOMMAX];     //
-char prenomDirecteur[PRENOMMAX];//
-//Adresse_t AdresseDirecteur;     //
-//Adresse_t AdresseEcole;         //
-int nbclasse;                   //
-struct Classe *tete;            //
+char nomEcole[NOMMAXECOLE];         //
+char nomDirecteur [NOMMAX];         //
+char prenomDirecteur[PRENOMMAX];    //
+//Adresse_t AdresseDirecteur;       //Non utilisé pour l'instant
+Adresse_t AdresseEcole;           //
+int nbclasse;                       //
+struct Classe *tete;                //
 }Ecole_t;
 
 
