0fc9fa4649cbd3e802f7f84f7486d03d69360356
elfedac <elfedac@gmail.com>
2017-01-28 23:27:09 +0100

    v6
    - Ajout d'un getchar() après la saisie de la date de naissance
    - Ajout ligne 85 et 90 (E->nomEleve[strlen(E->nomEleve)-1]='\0';) pour supprimer le \n généré automatiquement en fin de chaine par l'instruction fgets
    - Suppression du main de test

--- eleve.c
+++ eleve.c
@@ -1,16 +1,16 @@
-//*****************************************************************//
-//***           Fichiers eleve.c et eleve.h                     ***//
-//***              par Elvin et Manuel                          ***//
-//*****************************************************************//
+//*****************************************************************
+//***           Fichiers eleve.c et eleve.h                     ***
+//***              par Elvin et Manuel                          ***
+//*****************************************************************
 
 #include "eleve.h" //header eleve.h file called
 
 
 /* Student birthday input function */
-void SaisirDate(Eleve_t *Y)
+void SaisirDate(Eleve_t *E)
 {
     //Initialisation student birthday time structure
-    memset(&Y->DateNaissance, 0 , sizeof (Y->DateNaissance));
+    memset(&E->DateNaissance, 0 , sizeof (E->DateNaissance));
 
     //Localtime recovery
     time_t aujourdhui=time(NULL);
@@ -19,18 +19,19 @@ void SaisirDate(Eleve_t *Y)
 
     //Student birthday input
     printf ("\nSaisissez la date de naissance de l'élève (JJ/MM/AAAA): ");
-	scanf ("%d/%d/%d",	&Y->DateNaissance.tm_mday, &Y->DateNaissance.tm_mon, &Y->DateNaissance.tm_year);
-
-	// Time structure month and year modified before POSIX conversion
-	Y->DateNaissance.tm_mon-=1;
-    Y->DateNaissance.tm_year-=1900;
+    scanf ("%d/%d/%d",	&E->DateNaissance.tm_mday, &E->DateNaissance.tm_mon, &E->DateNaissance.tm_year);
+    getchar();
+	
+    // Time structure month and year modified before POSIX conversion
+    E->DateNaissance.tm_mon-=1;
+    E->DateNaissance.tm_year-=1900;
 
     //Student birthday converted in seconds
     birthday=mktime(&Y->DateNaissance);
 
     //Age is a double type variable, created & initialised to save student age through a calculation...
     double Age=difftime(aujourdhui, birthday)/(365.25*24*3600); //because of Leap year, a year is 365,25 days, a day is 24 hours & 1 hour is 3600 seconds
-    Y->age=Age;
+    E->age=Age;
 
 }
 
@@ -45,7 +46,7 @@ void AfficherEleve(Eleve_t E)
 
     //Display last name, first name & age
     printf("\nNom de l'eleve : %s\nPrenom de l'eleve : %s\nAge : %d ans\n",
-            E.nomEleve, E.prenomEleve, E.age);
+    E.nomEleve, E.prenomEleve, E.age);
 
     //Student Sex verification to display the true information
     if (E.sexe=='M')
@@ -59,15 +60,15 @@ void AfficherEleve(Eleve_t E)
 
   	printf ("\nDate de naissance : %d/%02d/%d\n", E.DateNaissance.tm_mday, 1 + E.DateNaissance.tm_mon, 1900 + E.DateNaissance.tm_year);
 
-  	//Student school year repeat verification to display the true information
+    //Student school year repeat verification to display the true information
     if (E.redoublant==1)
-        {
+    {
         printf("\nEleve redoublant\n");
-        }
+    }
     else
-        {
+    {
         printf("\nEleve non redoublant\n");
-        }
+    }
 
 }
 
@@ -81,10 +82,15 @@ void SaisirEleve(Eleve_t *E)
     //Last name student input
     printf("\nSaisissez le nom de l'eleve: ");
     fgets(E->nomEleve,NOMMAX,stdin);
-
+    E->nomEleve[strlen(E->nomEleve)-1]='\0';
+	
     //Fist name student input
     printf("\nSaisissez le prenom de l'eleve: ");
     fgets(E->prenomEleve,PRENOMMAX,stdin);
+    E->prenomEleve[strlen(E->prenomEleve)-1]='\0';
+	
+    //Input student birthday function call
+    SaisirDate(E);
 
     //Gender student input
     printf("\nSaisissez le Sexe de l'eleve (F / M): ");
@@ -92,7 +98,7 @@ void SaisirEleve(Eleve_t *E)
     getchar();
 
     //Gender student input check
-    while((E->sexe!='F') && (E->sexe!='M'))
+    while((E->sexe!='F') && (E->sexe!='f') && (E->sexe!='M') && (E->sexe!='m'))
 	{
 		CLEAR(); //if wrong input, system console screen cleared before displaying new choice
 		printf("\nJe n'ai pas compris...\nVeuillez re-saisir le sexe (F / M): ");
@@ -112,23 +118,5 @@ void SaisirEleve(Eleve_t *E)
 		printf("\nJe n'ai pas compris...\nL'élève est-il redoublant ? (1 pour redoublant/ 0 pour non redoublant): ");
 		scanf("%d",&E->redoublant);
     }
-
-    //Input student birthday function call
-    SaisirDate(E);
-}
-
-
-//test main a supprimer
-int main()
-{
-
-    // *** VARIABLES ***
-
-	Eleve_t  Elvin={0};
-
-    SaisirEleve(&Elvin);
-    AfficherEleve(Elvin);
-
-
 }
 
